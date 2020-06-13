6b3d05f017929cc33f030dd461c6930db171cc99
ahmadhassan997 <riemann997@gmail.com>
2020-06-13 22:00:44 +0500

    mongodb and mongoose

---
+++ node-express-mongo/node-mongo/index.js
@@ -0,0 +1,44 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dboper = require('./operations');
+
+const url  = 'mongodb://localhost:27017';
+const dbname = 'conFusion';
+
+const client = new MongoClient(url, {useUnifiedTopology: true});
+
+client.connect().then(() => {
+
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbname);
+    
+    dboper.insertDocument(db, { name: "ali", description: "chawal"}, 'dishes')
+    .then((result) => {
+        console.log("Insert Document: \n", result.ops);
+
+        return dboper.findDocuments(db, 'dishes');
+    })
+    .then((docs) => {
+        console.log("Found Documents\n", docs);
+
+        return dboper.updateDocument(db, { name: "ali"}, { description: "Less chawal"}, 'dishes')
+    })
+    .then((result) => {
+        console.log("Updated Document:\n", result.result);
+
+        return dboper.findDocuments(db, 'dishes')
+    })
+    .then((docs) => {
+        console.log("Found Documents\n", docs);
+                    
+        return db.dropCollection('dishes')
+    })
+    .then((result) => {
+        console.log('Dropped Collection: ', result);
+
+        client.close();
+    })
+    .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
